023240e8dead79dd13487798b81ca2d54d99932e
BrianLichliter <brian.lichliter@U.NORTHWESTERN.EDU>
2013-10-16 17:44:37 -0500

    Updated SQL queries

--- part3.pl
+++ part3.pl
@@ -1,18 +1,23 @@
-  eval { 
-    @rows = ExecSQL($dbuser, $dbpasswd, "select cmte_pty_affiliation, sum(transaction_amnt) from cs339.comm_to_cand natural join cs339.cmte_id_to_geo natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? group by cmte_pty_affiliation",undef,$latsw,$latne,$longsw,$longne);
-  };
   
+  ## [COMMITTEE TO CANDIDATE] returns total money and number of transactions within the area for cmte_string during $cycle_string
+  ## this operates on the committee to candidate table
   eval { 
-    @rows = ExecSQL($dbuser, $dbpasswd, "select cmte_pty_affiliation, sum(transaction_amnt) from cs339.comm_to_comm natural join cs339.cmte_id_to_geo natural join cs339.committee_master where($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? group by cmte_pty_affiliation",undef,$latsw,$latne,$longsw,$longne);
+    @rows = ExecSQL($dbuser, $dbpasswd, "select sum(transaction_amnt), count(transaction_amnt) from cs339.comm_to_cand natural join cs339.cmte_id_to_geo natural join cs339.committee_master where ($cycle_string) and ($cmte_string) and latitude>? and latitude<? and longitude>? and longitude<?",undef,$latsw,$latne,$longsw,$longne);
   };
+
+  ## [COMMITTEE TO INDIVIDUAL] returns total money and number of transactions within the area for cmte_string during $cycle_string
+  ## this operates on the committee to committee table
+  eval { 
+    @rows = ExecSQL($dbuser, $dbpasswd, "select sum(transaction_amnt), count(transaction_amnt) from cs339.comm_to_comm natural join cs339.cmte_id_to_geo natural join cs339.committee_master where ($cycle_string) and ($cmte_string) and latitude>? and latitude<? and longitude>? and longitude<?",undef,$latsw,$latne,$longsw,$longne);
+  };  
   
+  ## [INDIVIDUAL CONTRIBUTIONS] returns total money and number of transactions within the area for cmte_string during $cycle_string
+  ## this operates on the committee to candidate table
   eval {
-	@rows = ExecSQL($dbuser, $dbpasswd, "select cmte_pty_affiliation, sum(transaction_amnt) from cs339.ind_to_geo natural join cs339.individual natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? group by cmte_pty_affiliation",undef,$latsw,$latne,$longsw,$longne);
+	@rows = ExecSQL($dbuser, $dbpasswd, "select sum(transaction_amnt), count(transaction_amnt) from cs339.ind_to_geo natural join cs339.individual natural join cs339.committee_master where ($cycle_string) and ($cmte_string) and latitude>? and latitude<? and longitude>? and longitude<?",undef,$latsw,$latne,$longsw,$longne);
 };
 
+## [OPINIONS] returns average, standard deviation, and number of opinions within the area for opinions 
 eval {
-	@rows = ExecSQL($dbuser, $dbpasswd, "select avg(color), stddev(color) from rwb_opinions where latitude>? and latitude<? and longitude>? and longitude<?",undef,$latsw,$latne,$longsw,$longne);
+	@rows = ExecSQL($dbuser, $dbpasswd, "select avg(color), stddev(color), count(color) from rwb_opinions where latitude>? and latitude<? and longitude>? and longitude<?",undef,$latsw,$latne,$longsw,$longne);
 };
-
-#if rep + dem < tolerance
-#extend gps coordinates
