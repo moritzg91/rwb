da206d04288d13bff9f2ca61c5c2a7c748dac719
BrianLichliter <brian.lichliter@U.NORTHWESTERN.EDU>
2013-10-15 22:30:00 -0500

    built sql statements for part3
    test sql statements and statements to be incorporated into the perl for
    use in part 3

---
+++ part3.sql
@@ -0,0 +1,42 @@
+--
+--statements for the perl/COMM
+--
+select sum(transaction_amnt) from cs339.comm_to_cand natural join cs339.cmte_id_to_geo natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? and cmte_pty_affiliation = 'REP';
+
+select sum(transaction_amnt) from cs339.comm_to_cand natural join cs339.cmte_id_to_geo natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? and cmte_pty_affiliation = 'DEM';
+
+select sum(transaction_amnt) from cs339.comm_to_comm natural join cs339.cmte_id_to_geo natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? and cmte_pty_affiliation = 'REP';
+
+select sum(transaction_amnt) from cs339.comm_to_comm natural join cs339.cmte_id_to_geo natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? and cmte_pty_affiliation = 'DEM';
+
+--
+--testing statements/COMM
+--
+select sum(transaction_amnt) from cs339.comm_to_cand natural join cs339.committee_master natural join cs339.cmte_id_to_geo where cmte_pty_affiliation = 'DEM';
+
+select sum(transaction_amnt) from cs339.comm_to_cand natural join cs339.committee_master natural join cs339.cmte_id_to_geo where cmte_pty_affiliation = 'REP';
+
+select sum(transaction_amnt) from cs339.comm_to_comm natural join cs339.committee_master natural join cs339.cmte_id_to_geo where cmte_pty_affiliation = 'DEM';
+
+select sum(transaction_amnt) from cs339.comm_to_comm natural join cs339.committee_master natural join cs339.cmte_id_to_geo where cmte_pty_affiliation = 'REP';
+
+select stddev(transaction_amnt) from cs339.comm_to_comm natural join cs339.committee_master natural join cs339.cmte_id_to_geo where cmte_pty_affiliation = 'REP';
+
+--
+--statements for the perl/IND
+--
+select sum(transaction_amnt) from cs339.ind_to_geo natural join cs339.individual natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? and cmte_pty_affiliation = 'REP';
+
+select sum(transaction_amnt) from cs339.ind_to_geo natural join cs339.individual natural join cs339.committee_master where ($cycle_string) and latitude>? and latitude<? and longitude>? and longitude<? and cmte_pty_affiliation = 'DEM';
+
+--
+--testing statements/IND
+--
+select sum(transaction_amnt) from cs339.individual natural join cs339.committee_master natural join cs339.ind_to_geo where cmte_pty_affiliation = 'REP';
+
+select sum(transaction_amnt) from cs339.individual natural join cs339.committee_master natural join cs339.ind_to_geo where cmte_pty_affiliation = 'DEM';
+
+--SQL standard deviation function found here
+--http://www.techonthenet.com/oracle/functions/stddev.php
+--SQL average function found here
+--http://www.w3schools.com/sql/sql_func_avg.asp
